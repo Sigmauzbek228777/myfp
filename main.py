f74226973c50aad3fcbbfca1b3a4660334425cd6
Sigmauzbek228777 <Sgasanov048@gmail.com>
2026-08-04 15:47:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,22 +11,17 @@ ADMIN_ID = int(os.getenv("ADMIN_ID"))
 GOLDEN_KEY = os.getenv("GOLDEN_KEY")
 PROXY_URL = os.getenv("PROXY")
 
-
 bot = telebot.TeleBot(TOKEN)
 
 
 def send_alert(text):
     try:
-        bot.send_message(
-            ADMIN_ID,
-            f"🤖 [FunPay Bot]\n{text}"
-        )
+        bot.send_message(ADMIN_ID, f"🤖 [FunPay Bot]\n{text}")
     except Exception as e:
         print("Ошибка Telegram:", e)
 
 
 def funpay_worker():
-    print("Запуск FunPay...")
 
     proxy = None
 
@@ -42,46 +37,40 @@ def funpay_worker():
             proxy=proxy
         ).get()
 
-        print(f"Вход выполнен: {account.username}")
+        print("АККАУНТ:", account.username)
 
         send_alert(
-            f"✅ Бот запущен\n"
-            f"Аккаунт: {account.username}"
+            f"✅ Бот запущен\n{account.username}"
         )
 
     except Exception as e:
         print("Ошибка входа:", repr(e))
-
-        send_alert(
-            f"❌ Ошибка входа:\n{repr(e)}"
-        )
-
         return
 
 
     try:
-        print("Получение категорий...")
+        print("=== CATEGORY DEBUG ===")
 
         categories = account.get_sorted_categories()
 
-        print("====================")
-        print("СПИСОК КАТЕГОРИЙ:")
+        print("TYPE:")
+        print(type(categories))
 
-        for category in categories:
-            print(category)
+        print("DIR:")
+        print(dir(categories))
 
-        print("====================")
+        print("REPR:")
+        print(repr(categories))
+
+        print("=== END DEBUG ===")
 
         send_alert(
-            "✅ Категории выведены в Render."
+            "Категории проверены, смотри Render."
         )
 
     except Exception as e:
-        print("Ошибка категорий:", repr(e))
-
-        send_alert(
-            f"⚠ Ошибка категорий:\n{repr(e)}"
-        )
+        print("ОШИБКА:")
+        print(repr(e))
 
 
 class Handler(BaseHTTPRequestHandler):
@@ -89,9 +78,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(
-            b"Bot is running"
-        )
+        self.wfile.write(b"Bot is running")
 
     def do_HEAD(self):
         self.send_response(200)
@@ -105,21 +92,11 @@ if __name__ == "__main__":
         daemon=True
     ).start()
 
-
-    port = int(
-        os.getenv(
-            "PORT",
-            10000
-        )
-    )
+    port = int(os.getenv("PORT", 10000))
 
     server = HTTPServer(
         ("0.0.0.0", port),
         Handler
-    )
-
-    print(
-        f"Server started on {port}"
     )
 
     server.serve_forever()
